ccdbbd7c86383a190476b5634cbb8479d8afcbe3
ebilal01 <127566393+ebilal01@users.noreply.github.com>
2024-12-24 15:01:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
 import json
 import eventlet
 import random
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 # Setup Flask and SocketIO
 app = Flask(__name__, static_folder="static", template_folder="templates")
@@ -23,6 +23,11 @@ def generate_live_data():
                 "x": random.uniform(-5, 5),
                 "y": random.uniform(-5, 5),
                 "z": random.uniform(-5, 5)
+            },
+            "force": {
+                "x": random.uniform(-10, 10),
+                "y": random.uniform(-10, 10),
+                "z": random.uniform(-10, 10)
             }
         }
         socketio.emit("update_telemetry", json.dumps(telemetry_data))
@@ -35,6 +40,7 @@ def handle_connect():
 if __name__ == "__main__":
     socketio.start_background_task(generate_live_data)
     socketio.run(app, host="0.0.0.0", port=5000)
+
 
 
 
